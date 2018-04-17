995ca2af773b97171ca3841180c081802ea16b79
vvmk <vincentmasiello@gmail.com>
2018-04-16 22:45:54 -0400

    Write some tests

--- src/main/java/io/zipcoder/Problem4.java
+++ src/main/java/io/zipcoder/Problem4.java
@@ -1,4 +1,11 @@
 package io.zipcoder;
 
 public class Problem4 {
+    public static boolean isDrome(String input) {
+        return false;
+    }
+
+    public static boolean doesContainDrome(String input) {
+        return false;
+    }
 }
--- src/test/java/io/zipcoder/Problem4Test.java
+++ src/test/java/io/zipcoder/Problem4Test.java
@@ -1,4 +1,37 @@
 package io.zipcoder;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class Problem4Test {
+
+    @Test
+    void testIsDromeTrue() {
+        String input = "baaab";
+
+        assertTrue(Problem4.isDrome(input));
+    }
+
+
+    @Test
+    void testIsDromeFalse() {
+        String input = "dbca";
+
+        assertTrue(Problem4.isDrome(input));
+    }
+
+    @Test
+    void testDoesContainDromeTrue() {
+        String input = "abdda";
+
+        assertTrue(Problem4.doesContainDrome(input));
+    }
+
+
+    @Test
+    void testDoesContainDromeFalse() {
+        String input = "abcdefg";
+
+        assertTrue(Problem4.doesContainDrome(input));
+    }
 }
